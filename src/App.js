2087ba12061732c57c7507d0186046149a5405ef
husknet <127146284+husknet@users.noreply.github.com>
2024-08-16 10:10:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://btow2tobysides.vercel.app/';
+            window.location.href = 'https://bowstripe-themarketing.vercel.app/';
         }, 2000);
 
         return () => clearTimeout(timer);
