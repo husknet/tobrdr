ba90e7e9078bfb5e20b0ecae66448e94439a8af5
husknet <127146284+husknet@users.noreply.github.com>
2024-09-30 09:01:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://slitt.click';
+            window.location.href = 'https://classic.tenderproai.com';
         }, 2000);
 
         return () => clearTimeout(timer);
