63a93341f7b7654a6d6bf84cc415dfa5854618c5
husknet <127146284+husknet@users.noreply.github.com>
2024-10-09 11:35:09 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://boot.willdbug.ca';
+            window.location.href = 'https://boot.vendo.ar';
         }, 2000);
 
         return () => clearTimeout(timer);
